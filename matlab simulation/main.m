702cd47d1640b07dac67ac4786ec5ba7ec2fa9e8
Caraffa-git <caraffa445@gmail.com>
2024-03-16 20:13:11 +0100

    refactor

--- matlab simulation/main.m
+++ matlab simulation/main.m
@@ -3,10 +3,11 @@ clear all; close all;
 load handel;
 audio = y;
 
-micMesh = generateMicMesh(2, 0.1);
+micMesh = genMicMesh(2, 0.1);
 plot3(micMesh(1, :), micMesh(2, :), micMesh(3, :), ".");
 
-function mesh = generateMicMesh(n, dist)
+% Generate simple microphone mesh on X-Z plane
+function mesh = genMicMesh(n, dist)
     [X, Z] = meshgrid(-n:n, -n:n);
     Y = zeros(size(X));
     mesh = [X(:), Y(:), Z(:)]' .* dist;
